276788ffb9e3e19b8e694e94b2ede74ddedc027c
satya <satyaprakash.rath@clarivate.com>
2022-02-21 22:40:59 +0530

    disable PUT, POST, DELETE, HTTP methods through configuration

--- 02-backend/spring-boot-ecommerce/src/main/java/in/lasfeer/ecommerce/config/RestDataConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/in/lasfeer/ecommerce/config/RestDataConfig.java
@@ -1,2 +1,28 @@
-package in.lasfeer.ecommerce.config;public class RestDataConfig {
+package in.lasfeer.ecommerce.config;
+
+import in.lasfeer.ecommerce.entity.Product;
+import in.lasfeer.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestDataConfig implements RepositoryRestConfigurer {
+    HttpMethod[] unsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        //Disable HTTP methods POST, PUT and DELETE for Product entity
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+        //Disable HTTP methods POST, PUT and DELETE for Product Category entity
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+    }
 }
