1abded91cac78d983284da084ae93affefadc669
satya <satyaprakash.rath@clarivate.com>
2022-03-27 21:46:23 +0530

    search products by category part1

--- 02-backend/spring-boot-ecommerce/src/main/java/in/lasfeer/ecommerce/entity/ProductCategory.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/in/lasfeer/ecommerce/entity/ProductCategory.java
@@ -1,17 +1,20 @@
 package in.lasfeer.ecommerce.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "product_category")
+@Data
 public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
     @Column(name = "category_name")
-    private String category_name;
+    private String name;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
 }
